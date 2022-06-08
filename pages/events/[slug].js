2b7919d62b0315070b8c1e04edf12fe6fa555e50
giorgos_gr <giorgos@webdimension.gr>
2022-06-08 15:16:55 +0300

    getStaticPaths

--- pages/events/[slug].js
+++ pages/events/[slug].js
@@ -1,15 +1,50 @@
-import React from 'react'
-import Layout from '../../components/Layout'
+import React from "react";
+import Layout from "../../components/Layout";
+import { API_URL } from "@config/index";
 
-const EventPage = () => {
-
-
-
+const EventPage = ({ evt }) => {
   return (
     <Layout>
-       <h1>My event</h1> 
+      <h1>{evt.name}</h1>
     </Layout>
-  )
+  );
+};
+
+
+export async function getStaticPaths() {
+
+  const res = await fetch(`${API_URL}/api/events`)
+
+  const events = await res.json()
+
+  const paths = events.events.map(evt => ({params: {slug: evt.slug}}))
+
+  return {
+    paths,
+    fallback: 'blocking'
+  }
+
 }
 
-export default EventPage
+export async function getStaticProps({ params: { slug } }) {
+  const res = await fetch(`${API_URL}/api/events/${slug}`);
+
+  const events = await res.json();
+
+  return {
+    props: { evt: events.evt[0] },
+  };
+}
+
+
+// export async function getServerSideProps({ query: { slug } }) {
+//   const res = await fetch(`${API_URL}/api/events/${slug}`);
+
+//   const events = await res.json();
+
+//   return {
+//     props: { evt: events.evt[0] },
+//   };
+// }
+
+export default EventPage;
